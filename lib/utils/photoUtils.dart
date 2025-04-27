db7d17f8d3655295014464363973aaec72e023ff
samuelatas <875301@unizar.es>
2025-04-27 13:29:41 +0200

    foto invitado rival

--- lib/utils/photoUtils.dart
+++ lib/utils/photoUtils.dart
@@ -1,5 +1,5 @@
 String getRutaSeguraFoto(String? fotoGuardada) {
-  if (fotoGuardada == null || fotoGuardada.isEmpty || fotoGuardada.endsWith('/none')) {
+  if (fotoGuardada == null || fotoGuardada.isEmpty || fotoGuardada.endsWith('/none') || fotoGuardada == "none") {
     return 'assets/fotosPerfil/fotoPerfil.png';
   }
 
